e8255562b9f8daa0c852eb40b0dd913c09f3b11d
erdoganabaci <erdoganabaci97@gmail.com>
2022-08-22 22:52:37 +0200

    Add phrase for test sentences

--- linprog.pl
+++ linprog.pl
@@ -28,9 +28,9 @@ equals --> [is].
 
 equals --> [equals].
 equals --> [equal].
-equals --> [greater],[than],[or].
+equals --> [greater],[than],[or],[equal],[to].
 equals --> [contains] .
-equals -> [holds].
+equals --> [holds].
 
 
 subject(OldVars, NewVars, Var) --> variable ,[X], {lookup(X, OldVars, NewVars, Var)}.
@@ -40,8 +40,7 @@ variable --> [a], [variable].
 variable --> [variable].
 variable --> [].
 
-% sentences(OldVars, NewVars) --> sentence(OldVars, NewVars).
-% sentences(OldVars, NewVars) --> sentence(OldVars, NewVars), sentences(OldVars, NewVars).
+
 
 sentence(OldVars, NewVars) --> subject(OldVars, NewVars, Var), between, [Number1], and, [Number2], [fullstop],
 {(Number1 #> Number2) #==> (Var #>= Number2 #/\ Var #=< Number1), (Number1 #=< Number2) #==> (Var #>= Number1 #/\ Var #=< Number2), integer(Number1), integer(Number2)}.
@@ -51,35 +50,27 @@ sentence(OldVars, NewVars) --> subject(OldVars, InterimVars, Var), equals, math_
 
 sentence(OldVars, NewVars) --> subject(OldVars, InterimVars, Var), equals, math_calculation(InterimVars, NewVars, Result),[fullstop],
 {Var #>= Result}.
-% All these variables are greater than 5.
-% sentence(OldVars, NewVars, Var) --> subject(OldVars, InterimVars, Var), equals, math_calculation(InterimVars, NewVars, Result),
-% {Var #= Result}.
 
 
 
-math_calculation(OldVars, NewVars, Result) --> [A], [plus], [B], {lookup(A, OldVars, InterimVars, VarA), 
-lookup(B, InterimVars, NewVars, VarB),
-Result is VarA + VarB
+math_calculation(OldVars, NewVars, VarA + VarB) --> [A], [plus], [B], {lookup(A, OldVars, InterimVars, VarA), 
+lookup(B, InterimVars, NewVars, VarB)
 }.
-math_calculation(OldVars, NewVars, Result) --> [the],[product],[of],[A], [and], [B], {
+math_calculation(OldVars, NewVars, VarA * VarB) --> [the],[product],[of],[A], [and], [B], {
 lookup(A, OldVars, InterimVars, VarA), 
-lookup(B, InterimVars, NewVars, VarB),
-Result is VarA * VarB
+lookup(B, InterimVars, NewVars, VarB)
 }.
-math_calculation(OldVars, NewVars, Result) --> [C], [times], [Number],{
-lookup(C, OldVars, InterimVars, VarC),
-Result is VarC * Number
+math_calculation(OldVars, NewVars, VarC * Number) --> [C], [times], [Number],{
+lookup(C, OldVars, InterimVars, VarC)
 }.
 
-math_calculation(OldVars, NewVars, Result) --> [quotient],[of],[Z], [and], [Number],{
-lookup(Z, OldVars, InterimVars, VarZ),
-Result is VarZ / Number
+math_calculation(OldVars, NewVars, VarZ / Number) --> division,[Z], [and], [Number],{
+lookup(Z, OldVars, InterimVars, VarZ)
 }.
 
-% front end: lpsolve/2 succeeds when its first
-% argument is a list of well formed sentences
-% and its second is a list of constrained variables
-% lpsolve([the,variable,x,lies,between,0,and,10,fullstop,variable,x,varies,from,1,to,20,fullstop],Answers).
+division --> [quotient],[of].
+division --> [the],[dividend],[of].
+
 
 lpsolve( Data, Answers ) :-
     lpsolve( [], Answers, Data, [] ).
@@ -90,25 +81,19 @@ lpsolve(OldVars,FutureVars,Data,FutureRemain) :-
      phrase(sentence( OldVars,NewVars),Data,Remain),
      lpsolve(NewVars,FutureVars,Remain,FutureRemain).
 
-% lpsolve/4 succeeds when its first argument is a
-% list of variable name/Prolog variable pairs, its
-% second is the same list modified with any new
-% variables introduced in the list of sentences
-% in argument 3 which is parsed with the remainder
-% given in argument 4
+% lpsolve([the,variable,x,lies,between,0,and,10,fullstop,variable,x,varies,from,1,to,20,fullstop],Answers).
+% phrase(sentence(OldVars, NewVars),[the,variable,x,lies,between,0,and,10,fullstop]).
+% phrase(sentence(OldVars, NewVars),[variable,x,varies,from,1,to,20,fullstop]).
+% phrase(sentence(OldVars, NewVars),[a,variable,x,is, in,the,range,100,to,14,fullstop]).
 
 
-% phrase(start(OldVars, NewVars),[the,variable,x,lies,between,0,and,10]).
-
-% phrase(sentence(OldVars, NewVars),[the,variable,x,lies,between,0,and,10]).
-% Variable x varies from 1 to 20.
-% phrase(start(OldVars, NewVars),[the,variable,x,lies,between,0,and,10,fullstop,variable,x,varies,from,1,to,20,fullstop]).
-% phrase(start(OldVars, NewVars),[the,variable,x,lies,between,0,and,10,variable,y,varies,from,1,to,20]).
 % The variable x lies between 0 and 10.
-
-% phrase(sentence(OldVars, NewVars, Var),[the,variable,x,lies,between,0,and,10]).
-%% Remain sentences 
-
+% Variable x varies from 1 to 20.
+% A variable x is in the range 100 to 14.
+% y is between -2 and 25.
+% x equals a plus b.
+% x is c times 2.
+% The variable z contains the product of b and c.
 % y is less than 5 + 2 * q.
 % All these variables are greater than 5.
 % Variable q is greater than or equal to the quotient of z and 2.
@@ -116,27 +101,3 @@ lpsolve(OldVars,FutureVars,Data,FutureRemain) :-
 % It is greater than q.
 
 
-% Here are some notes on the sentences:
-% • The scope of the variable names is the entire set of input sentences { that is, once a variable
-% is named, any use of its name refers to the same variable.
-
-% • The referring expression It refers to the variable which was the subject of the previous
-% sentence, unless the previous sentence began with All, in which case an exception is generated
-% (see the manual for details).
-
-% • The referring expression All these variables refers to the set of variables which have been
-% named up until its occurrence in the input string. It does not refer to any variables named
-% after it in the input string.
-% 2
-
-% • The noun phrase A variable x always introduces a new variable called x; using it twice with
-% the same variable name should generate an exception, because this would cause a scoping
-% error.
-
-% Do not forget that mathematical expressions are not simply evaluated in left to right order { you
-% have to allow for the fact that +/- and  X and  / bind their arguments differently.
-% Make any assumptions which you suppose in designing your grammar clear in your comments.
-
-
-
-% lookup/4 predicate will create
